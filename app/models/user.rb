a3a1c877f87b82765b135fb0ae412e1720c01fab
Ota66 <mitu.xxo66@gmail.com>
2021-03-09 16:55:39 +0900

    tweetsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # has_many :room_users
   # has_many :rooms, through: room_users
   # has_many :messages
-  # has_many :tweets
+  has_many :tweets
   # has_many :comments
 
   with_options presence: true do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_093244) do
+ActiveRecord::Schema.define(version: 2021_03_09_074342) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_03_08_093244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
