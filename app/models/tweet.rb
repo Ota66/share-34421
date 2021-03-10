2f6892d065ebf8b28ca62b94e7ac763ce53e6538
Ota66 <mitu.xxo66@gmail.com>
2021-03-10 15:13:32 +0900

    画像のみでも送信可能に設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,9 @@ class Tweet < ApplicationRecord
   # has_many :comments
   has_one_attached :image
 
-  validates :text, presence: true
+  validates :text, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
