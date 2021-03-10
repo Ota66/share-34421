4523c88fc98a1b31bd5f24f1c415ef350d7d0096
Ota66 <mitu.xxo66@gmail.com>
2021-03-10 17:35:50 +0900

    ツイート削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,12 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
