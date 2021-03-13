fcd99e61e79fcf24f1e11e3ad1105bf0995b7f9a
Ota66 <mitu.xxo66@gmail.com>
2021-03-13 15:41:02 +0900

    ダークモード機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    # binding.pry
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order("created_at DESC")
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:introduction, :nickname)
+  end
+
 
 end
