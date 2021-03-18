d4545b568d4a7da9e4a0cea59eaeb3015d949204
Ota66 <mitu.xxo66@gmail.com>
2021-03-18 17:56:04 +0900

    tweet,commentモデル単体テスト

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :tweet do
+    text   {'初投稿'}
+    video { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/A-3.mov')) }
+    association :user
     
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('app/assets/images/icon-250.png'), filename: 'icon-250.png')
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,6 @@ FactoryBot.define do
     email {Faker::Internet.free_email}
     password { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation {password}
-    # introduction {'はじめまして'}
-
-    # after(:build) do |user|
-    #   user.avatar.attach(io: File.open('app/assets/images/icon-250.png'), filename: 'icon-250.png')
-    # end
-
-    # after(:build) do |user|
-    #   user.header_image.attach(io: File.open('app/assets/images/icon-250.png'), filename: 'icon-250.png')
-    # end
+    introduction {'はじめまして'}
   end
 end
