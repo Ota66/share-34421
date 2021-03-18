69c550615b3a2aa08c826d88d10fa2bc6017b577
Ota66 <mitu.xxo66@gmail.com>
2021-03-18 12:09:54 +0900

    動画ビューに表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,10 @@
 class LikesController < ApplicationController
 
+  def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @likes = Like.all
+  end
+
   def create
     @like = current_user.likes.create(tweet_id: params[:tweet_id])
     redirect_back(fallback_location: root_path)
